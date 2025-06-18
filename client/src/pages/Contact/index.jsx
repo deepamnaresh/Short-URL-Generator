0aa7dd108e9eaa3ca64f35015eee4c2528b85eb9
deepamnaresh <107993540+deepamnaresh@users.noreply.github.com>
2025-06-18 15:17:58 +0530

    Update index.jsx

--- client/src/pages/Contact/index.jsx
+++ client/src/pages/Contact/index.jsx
@@ -1,10 +1,18 @@
 import ContactUs from './ContactUs';
+import Sidebar from '../components/Sidebar';
 
 function App() {
   return (
-    <div className='contactus mt-40%'>
-      <ContactUs />
-    </div>
+    <div className="flex">
+  <div className="w-1/4">
+    <Sidebar />
+  </div>
+  <div className="w-3/4">
+    <ContactUs />
+  </div>
+</div>
+
+    
   );
 }
 
