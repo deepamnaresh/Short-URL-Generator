61bb2810b39409e7e668671994c1d84a6c32c21b
deepamnaresh <107993540+deepamnaresh@users.noreply.github.com>
2025-06-18 22:22:24 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -21,15 +21,10 @@ const app = express()
 
 //middlewares
 
-app.options('*', cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-}));
-
-// app.use(cors({
-//     origin: process.env.CLIENT_URL||"http://localhost:5173",
-//     credentials:true
-// }))
+app.use(cors({
+    origin: process.env.CLIENT_URL||"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json({limit:"50kb"}))
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true,limit:"50kb"}))
